24f60ec04992c5cd10ad318e5136f2a11621af4d
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2022-09-20 20:54:36 +0530

    Written the code

---
+++ PrimeCheckAgain.js
@@ -0,0 +1,26 @@
+
+// Problem is to check the number is Prime or not but the condition is not to use the BRUTE FORCE
+// you have to make the code optimum so that it can take less time and space 
+
+
+
+
+function checkPrime(num) {
+
+    if(num <= 1 || num == undefined) {
+        return false;
+    }
+    for(let i = 2; i<=num**0.5; i++){
+        if(num%i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+let num = 11;
+
+if(checkPrime(num)){
+    console.log("It is a Prime number");
+}else {
+    console.log("Not a Prime number");
+}
